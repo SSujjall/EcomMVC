c231691ddfbc57fd010c213b0a17aa75e8ca32f2
Sujal Maharjan <sum0590@my.londonmet.ac.uk>
2024-10-03 11:58:28 +0545

    Login and Registration model validation added

--- Models/DTOs/LoginDTO.cs
+++ Models/DTOs/LoginDTO.cs
@@ -1,10 +1,16 @@
 ﻿using EcomSiteMVC.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcomSiteMVC.Models.DTOs
 {
     public class LoginDTO
     {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, ErrorMessage = "Username cannot exceed 50 characters")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be atleast 3 characters")]
         public string PasswordHash { get; set; }
     }
 }
