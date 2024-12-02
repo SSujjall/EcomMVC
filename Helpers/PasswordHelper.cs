de7a1742b5cbdba990b9ff7ed65a42b08de61c63
Sujal Maharjan <sum0590@my.londonmet.ac.uk>
2024-12-02 14:05:39 +0545

    Password Helper Segregated, Admin login view updated

---
+++ Helpers/PasswordHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace EcomSiteMVC.Helpers
+{
+    public static class PasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string enteredPassword, string storedPasswordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(enteredPassword, storedPasswordHash);
+        }
+    }
+}
