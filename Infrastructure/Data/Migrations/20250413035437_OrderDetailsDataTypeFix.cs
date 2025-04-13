47b9a2e91e2ed8c51e88139bd826bf35d693bf49
SSujjall <suzallmhz@gmail.com>
2025-04-13 09:45:07 +0545

    DataType Fix for Order Details

---
+++ Infrastructure/Data/Migrations/20250413035437_OrderDetailsDataTypeFix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcomSiteMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderDetailsDataTypeFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalAmount",
+                table: "OrderDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "PasswordHash" },
+                values: new object[] { new DateOnly(2025, 4, 13), "$2a$11$Mvaq1VX1/IJRW2.TkwUYXuJBxPN7GuG08wAoLuCURdzf1.cAInDrW" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TotalAmount",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "PasswordHash" },
+                values: new object[] { new DateOnly(2025, 2, 4), "$2a$11$bzX8qyDu8Dyjjb3dKhm3PeHqo25/yvrrlWkwhp8BCA39GLAVt7sra" });
+        }
+    }
+}
